06738844ff82a28c7ee0da8059324d052139527a
Tomaeto <65134185+Tomaeto@users.noreply.github.com>
2023-09-06 20:38:41 -0400

    Delete functions directory

--- functions/diffArray.js
+++
@@ -1,8 +0,0 @@
-//Function for returning an array of the unique values in arr1 and arr2
-//Filters each array for only the values that are not in the other array and returns array with those values
-function diffArray(arr1, arr2) {
-  const newArr1 = arr1.filter(val => arr2.indexOf(val) < 0);
-  const newArr2 = arr2.filter(val => arr1.indexOf(val) < 0);
-  return [...newArr1, ...newArr2]
-  
-}
--- functions/dropElements.js
+++
@@ -1,6 +0,0 @@
-//Function for dropping elements from arr while func(arr[0]) is false
-//Shifts arr until func returns true or arr becomes empty
-function dropElements(arr, func) {
-  while (arr.length > 0 && !(func(arr[0]))) arr.shift();
-  return arr;
-}
