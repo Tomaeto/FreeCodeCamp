e7497bbfd2e57178e4d5b831b22d879c8ed32932
Adrian Faircloth <adrianspenser@gmail.com>
2023-09-11 16:33:38 -0400

    Added function w/ chained middleware

--- Backend and API/basic node and express/myApp.js
+++ Backend and API/basic node and express/myApp.js
@@ -11,18 +11,24 @@ app.use(function middleware(req, res, next) {
     console.log(str);
     next();
 });
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
-})
+});
 
 app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE == 'uppercase')
     res.json({"message": "HELLO JSON"});
     else   
         res.json({"message": "Hello json"});
-})
+});
 
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+    }, function(req, res) {
+        res.json({time: req.time});
+});
 
 
 
