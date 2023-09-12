b8545c36978f21168c034c1ef10a5b91ea262706
Adrian Faircloth <adrianspenser@gmail.com>
2023-09-12 15:32:11 -0400

    Added Person schema

--- Backend and API/mongodb and mongoose/myApp.js
+++ Backend and API/mongodb and mongoose/myApp.js
@@ -3,7 +3,14 @@ let mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI);
 
-let Person;
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: {type: Number},
+  favoriteFoods: {type: [String]}
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
