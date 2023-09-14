07c10894a23007962a92d0fe4ed8760ff1bbb866
Adrian Faircloth <adrianspenser@gmail.com>
2023-09-14 12:50:30 -0400

    Completed Header Parser project

--- Backend and API/projects/header parser/index.js
+++ Backend and API/projects/header parser/index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//whoami API endpoint, returns JSON w/ request's IP, language, and software
+app.get('/api/whoami', (req, res) => {
+  let result = { "ipaddress": req.ip,
+              "language": req.headers['accept-language'],
+              "software": req.headers['user-agent']};
+      res.json(result);
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
