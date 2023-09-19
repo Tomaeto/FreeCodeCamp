ece1f185c907985735edd48f62b2450fdfd088b7
Adrian Faircloth <adrianspenser@gmail.com>
2023-09-19 16:45:07 -0400

    Completed File Metadata project, completed course

--- Backend and API/projects/file metadata/index.js
+++ Backend and API/projects/file metadata/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    'name': req.file.originalname,
+    'type': req.file.mimetype,
+    'size': req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
